c7ccfcecd91c6f07a0a6be8cc109fc7c45f783af
Ronaldi <ronaldisuwanto@gmail.com>
2020-03-09 19:11:42 +0700

    to be continued bab 5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,10 +20,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
     end
+  
+    test "should get contact" do
+      get static_pages_contact_url
+      assert_response :success
+      assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    end
+     
     
 
 end
